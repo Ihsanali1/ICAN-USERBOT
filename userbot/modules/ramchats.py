5c4c90b77ec49c8e034ef535db220704c20fe123
Ihsanali1 <96628046+Ihsanali1@users.noreply.github.com>
2021-12-25 01:31:52 +0700

    Update ramchats.py

--- userbot/modules/ramchats.py
+++ userbot/modules/ramchats.py
@@ -108,7 +108,22 @@ async def log(log_text):
 @register(outgoing=True, pattern="^.exit$")
 async def kickme(leave):
     """ Basically it's .kickme command """
-    await leave.edit(f"goodbyee"
+    await leave.edit(f"▬▬▬.◙.▬▬▬ \n"
+                     "═▂▄▄▓▄▄▂ \n"
+                     "◢◤ █▀▀████▄▄▄▄◢◤ \n"
+                     "█▄ █ █▄ ███▀▀▀▀▀▀▀╬ \n"
+                     "◥█████◤ \n"
+                     "══╩══╩══ \n"
+                     "╬═╬ \n"
+                     "╬═╬ \n"
+                     "╬═╬ \n"
+                     "╬═╬ \n"
+                     "╬═╬ \n"
+                     "╬═╬ \n"
+                     "╬═╬ AKU PERGI YA NGENTOT, BABAY! \n"
+                     "╬═╬☻/ \n"
+                     "╬═╬/▌ \n"
+                     "╬═╬/ \\ \n")
     sleep(10)
     await leave.edit(f"`{ALIVE_NAME}, Meninggalkan Group ini, Good bye evrybody`")
     await leave.client.kick_participant(leave.chat_id, 'me')
